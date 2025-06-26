88bb07a1fc6bf0df1c412eca7b7f05a186e830ce
mohammed-sarhad-ahmed <120845827+mohammed-sarhad-ahmed@users.noreply.github.com>
2025-06-26 23:01:07 +0300

    Add updateMe route to auth routes
    Introduces a new PATCH /update-me endpoint to allow authenticated users to update their information. The route is protected and uses the updateMe handler.

--- routes/auth_route.js
+++ routes/auth_route.js
@@ -5,7 +5,8 @@ const {
   forgotPassword,
   resetPassword,
   updateMyPassword,
-  protectRoute
+  protectRoute,
+  updateMe
 } = require('../handlers/auth_handler');
 
 const Router = express.Router();
@@ -16,5 +17,7 @@ Router.post('/login', login);
 Router.post('/forgot-password', forgotPassword);
 Router.patch('/reset-password/:token', resetPassword);
 Router.patch('/update-my-password', protectRoute, updateMyPassword);
+// Router.patch('/update-my-email', protectRoute, updateMyEmail);
+Router.patch('/update-me', protectRoute, updateMe);
 
 module.exports = Router;

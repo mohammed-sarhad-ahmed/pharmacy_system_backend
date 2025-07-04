ccecff5d556e921ac04199aa2116e863bd068f70
mohammed-sarhad-ahmed <120845827+mohammed-sarhad-ahmed@users.noreply.github.com>
2025-07-04 22:27:29 +0300

    Add order model schema with references
    Introduces a new Mongoose schema for orders, including references to medicines, pharmacy, and supplier. Ensures at least one medicine is present in each order and adds timestamps.

---
+++ model/order.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const orderSchema = mongoose.Schema(
+  {
+    medicines: {
+      type: [
+        {
+          type: mongoose.Types.ObjectId,
+          required: [true, 'Medicine id is required'],
+          ref: 'Medicine'
+        }
+      ],
+
+      validation: {
+        validator: (arr) => Array.isArray(arr) && arr.length > 0,
+        message: 'At least one medicine has to be in an order'
+      }
+    },
+    pharmacy: {
+      type: mongoose.Types.ObjectId,
+      required: [true, 'Pharmacy id is required'],
+      ref: 'Pharmacy'
+    },
+    supplier: {
+      type: mongoose.Types.ObjectId,
+      required: [true, 'Supplier id is required'],
+      ref: 'Supplier'
+    }
+  },
+  {
+    timestamps: true
+  }
+);
